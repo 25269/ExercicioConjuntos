d690842ef20914b78ba5524b8ba29a63c4e3053f
Cassiano Baldasso <baldassocassiano@gmail.com>
2024-10-30 21:45:12 -0300

    Versão final

---
+++ Conjuntos/Entities/Course.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conjuntos.Entities
+{
+    internal class Course
+    {
+        public string NameCourse { get; set; }
+        public int CodeStudentEnrolled { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Course))
+            {
+                return false;
+            }
+            Course other = obj as Course;
+
+            return CodeStudentEnrolled.Equals(other.CodeStudentEnrolled);
+        }
+
+    }
+}
---
+++ Conjuntos/Program.cs
@@ -0,0 +1,76 @@
+﻿using Conjuntos.Entities;
+
+namespace Conjuntos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int codeStudent;
+            int follow;
+
+            HashSet<Course> studentsCourses = new HashSet<Course>();
+
+            Console.Write("To enter the system, type 1: ");
+            follow = int.Parse(Console.ReadLine());
+
+            while (follow == 1)
+            {
+                Console.Write("Which course do you want to enroll students in? ");
+                string enrolledCourse = Console.ReadLine();
+
+                switch (enrolledCourse)
+                {
+                    case "A":
+                        Console.Write("How many students for course A? ");
+                        int totalStudentsA = int.Parse(Console.ReadLine());
+
+                        for (int i = 0; i < totalStudentsA; i++)
+                        {
+                            codeStudent = int.Parse((Console.ReadLine()));
+                                                        
+                            studentsCourses.Add(new Course{ NameCourse = enrolledCourse, CodeStudentEnrolled = codeStudent});
+                        }
+                        Console.WriteLine();
+                        break;
+
+                    case "B":
+                        Console.Write("How many students for course B? ");
+                        int totalStudentsB = int.Parse(Console.ReadLine());
+
+                        for (int i = 0; i < totalStudentsB; i++)
+                        {
+                            codeStudent = int.Parse((Console.ReadLine()));
+
+                            studentsCourses.Add(new Course { NameCourse = enrolledCourse, CodeStudentEnrolled = codeStudent });
+                        }
+                        Console.WriteLine();
+                        break;
+
+                    case "C":
+                        Console.Write("How many students for course C? ");
+                        int totalStudentsC = int.Parse(Console.ReadLine());
+
+                        for (int i = 0; i < totalStudentsC; i++)
+                        {
+                            codeStudent = int.Parse((Console.ReadLine()));
+
+                            studentsCourses.Add(new Course { NameCourse = enrolledCourse, CodeStudentEnrolled = codeStudent });
+                        }
+                        Console.WriteLine();
+                        break;
+                }
+
+                follow = 0;
+
+                Console.Write("To continue in the system, type 1. If you want to exit, type 0: ");
+                follow = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+            Console.Write("Total students: " + studentsCourses.Count);
+            Console.WriteLine();
+        }
+    }
+}
+
